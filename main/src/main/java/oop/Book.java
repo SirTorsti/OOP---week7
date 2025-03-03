31aef6422e1b98b2f8058124840eb081bb4a1274
Hyphen <tommi.heinonen@student.lut.fi>
2025-03-03 22:36:55 +0200

    slight changes

--- main/src/main/java/oop/Book.java
+++ main/src/main/java/oop/Book.java
@@ -6,6 +6,10 @@ public abstract class Book {
     int copies;
 
     public void displayInfo() {
-        System.out.println("Nimi: " + title + "\nKirjailija: " + author + "\nSivut: " + pages + "\nMäärä: " + copies);
+        System.out.println("Nimi: " + title);
+        System.out.println("Kirjailija " + author);
+        System.out.println("Sivut: " + pages);
+        System.out.println("Määrä: " + copies);
     }
+    public abstract String categorize();
 }

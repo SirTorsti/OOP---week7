6d2eb5c3cd4df88289e1e08362077e7d9144b275
Hyphen <tommi.heinonen@student.lut.fi>
2025-03-03 22:38:36 +0200

    I was right all along about the print in displayInfo, just misunderstood feedback

--- main/src/main/java/oop/Book.java
+++ main/src/main/java/oop/Book.java
@@ -6,10 +6,7 @@ public abstract class Book {
     int copies;
 
     public void displayInfo() {
-        System.out.println("Nimi: " + title);
-        System.out.println("Kirjailija " + author);
-        System.out.println("Sivut: " + pages);
-        System.out.println("Määrä: " + copies);
+        System.out.println("Nimi: " + title + "Kirjailija " + author + "Sivut: " + pages + "Määrä: " + copies);
     }
     public abstract String categorize();
 }
